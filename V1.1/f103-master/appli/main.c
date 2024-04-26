b4c28fa086e296464e80e0a9d9f1f75cbc181285
Ask1ew <99717881+Ask1ew@users.noreply.github.com>
2024-04-26 11:36:50 +0200

    V1.0 et V1.1

---
+++ V1.1/f103-master/appli/main.c
@@ -0,0 +1,119 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Nirgal
+  * @date    03-July-2019
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+#include "stm32f1xx_hal.h"
+#include "stm32f1_uart.h"
+#include "stm32f1_sys.h"
+#include "stm32f1_gpio.h"
+#include "macro_types.h"
+#include "systick.h"
+
+void writeLED(bool_e b)
+{
+	HAL_GPIO_WritePin(LED_GREEN_GPIO, LED_GREEN_PIN, b);
+}
+
+bool_e readButton(void)
+{
+	return !HAL_GPIO_ReadPin(BLUE_BUTTON_GPIO, BLUE_BUTTON_PIN);
+}
+
+static void MX_GPIO_Init(void){
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_USART2_CLK_ENABLE();
+    __HAL_RCC_USART3_CLK_ENABLE();
+
+    // Configuration des broches UART2
+    GPIO_InitStruct.Pin = GPIO_PIN_2;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_3;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    // Configuration des broches UART3
+    GPIO_InitStruct.Pin = GPIO_PIN_10;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_11;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+}
+
+static void MX_USART2_UART_Init(void){
+    huart2.Instance = USART2;
+    huart2.Init.BaudRate = 9600;
+    huart2.Init.WordLength = UART_WORDLENGTH_8B;
+    huart2.Init.StopBits = UART_STOPBITS_1;
+    huart2.Init.Parity = UART_PARITY_NONE;
+    huart2.Init.Mode = UART_MODE_TX_RX;
+    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+    if (HAL_UART_Init(&huart2) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+static void MX_USART3_UART_Init(void){
+    huart3.Instance = USART3;
+    huart3.Init.BaudRate = 9600;
+    huart3.Init.WordLength = UART_WORDLENGTH_8B;
+    huart3.Init.StopBits = UART_STOPBITS_1;
+    huart3.Init.Parity = UART_PARITY_NONE;
+    huart3.Init.Mode = UART_MODE_TX_RX;
+    huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart3.Init.OverSampling = UART_OVERSAMPLING_16;
+    if (HAL_UART_Init(&huart3) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+int main(void)
+{
+	HAL_Init();
+
+	UART_init(UART2_ID,115200);
+
+	SYS_set_std_usart(UART2_ID, UART2_ID, UART2_ID);
+
+	MX_GPIO_Init();
+	MX_USART2_UART_Init(); // Initialisation de l'UART2 pour le module Bluetooth
+	MX_USART3_UART_Init();
+
+	//Initialisation du port de la led Verte (carte Nucleo)
+	BSP_GPIO_PinCfg(LED_GREEN_GPIO, LED_GREEN_PIN, GPIO_MODE_OUTPUT_PP,GPIO_NOPULL,GPIO_SPEED_FREQ_HIGH);
+
+	//Initialisation du port du bouton bleu (carte Nucleo)
+	BSP_GPIO_PinCfg(BLUE_BUTTON_GPIO, BLUE_BUTTON_PIN, GPIO_MODE_INPUT,GPIO_PULLUP,GPIO_SPEED_FREQ_HIGH);
+
+	//On ajoute la fonction process_ms à la liste des fonctions appelées automatiquement chaque ms par la routine d'interruption du périphérique SYSTICK
+	Systick_add_callback_function(&process_ms);
+
+	uint8_t gpsData[100];
+
+	while(1)	//boucle de tâche de fond
+	{
+		HAL_UART_Receive(&huart3, gpsData, sizeof(gpsData), HAL_MAX_DELAY);
+
+		        // Envoyer les données GPS lues au module Bluetooth
+		HAL_UART_Transmit(&huart2, gpsData, sizeof(gpsData), HAL_MAX_DELAY);
+
+	}
+}
